5f8605b280c0b5452d5b8d654d7922cb7b4a2ace
Fabian Kunze <fkunze@fkunze.de>
2023-10-13 20:07:09 +0200

    remove hardcoded url that forces on-compile existance of level data.

--- src/data/levels.rs
+++ src/data/levels.rs
@@ -1,14 +1,14 @@
 use super::level_state::LevelState;
 
-macro_rules! level_path {
-    ($rel_path:literal) => {
-        concat!("../../burrows/Demo_Remake_v1.1.2/", $rel_path)
-    };
-}
+// macro_rules! level_path {
+//     ($rel_path:literal) => {
+//         concat!("../../burrows/Demo_Remake_v1.1.2/", $rel_path)
+//     };
+// }
 
-pub fn level() -> LevelState {
-    let mut res = LevelState::new();
-    res.parse_level(include_str!(level_path!("DemoCenter/1.level")))
-        .expect("valid");
-    res
-}
+// pub fn level() -> LevelState {
+//     let mut res = LevelState::new();
+//     res.parse_level(include_str!(level_path!("DemoCenter/1.level")))
+//         .expect("valid");
+//     res
+// }
